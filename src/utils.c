424d76060099bb19ddefc4362a6f1c9395a99ccc
Will Sayin <william.sayin@gmail.com>
2019-08-23 11:16:45 -0700

    Small bug in strtouint64_t, forgot to increment str pointer after first digit.

--- src/utils.c
+++ src/utils.c
@@ -1,43 +1,13 @@
 #include <ctype.h>
 #include <stdint.h>
 
-char *token( char *pch, char *tok )
-{
-  /* skip leading spaces */
-  while (isspace(*pch) || iscntrl(*pch))
-    pch++; 
-
-  /* copy the beginning of the string */
-  while (*pch != '\0')
-  {
-    /* have we reached the end of the first word ? */
-    if( isspace( *pch ) || iscntrl( *pch ) )
-    {
-      pch++;
-      break;
-    }
-
-    /* copy one char */
-    *tok++ = *pch++;
-  }
-
-  /* terminate string */
-  *tok = '\0';
-
-  /* skip past any leftover spaces */
-  while( isspace( *pch ) || iscntrl( *pch ) )
-    pch++;
-
-  /* return the leftovers */
-  return pch;
-}
-
 uint64_t strtouint64_t( char *str )
 {
    uint64_t ret = 0;
    while( !isdigit( *str ) )
       str++;
    ret = *str - '0';
+   str++;
    while( isdigit( *str ) )
    {
       ret *= 10;
